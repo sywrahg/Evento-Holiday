d5c3f2e4518197dca35a66f58c6383d7f65842d5
fabiocardosojr <fabio.cardosojr@gmail.com>
2017-08-28 15:43:03 -0300

    Update Classe Cupom
    Adicionando funcionalidades à classe Cupom:
    
    Desconto;
    Validação de código do cupom;
    
    Adicionada também CupomMain.class para visualizar o funcionamento da clase.

--- src/models/Cupom.java
+++ src/models/Cupom.java
@@ -1,21 +1,83 @@
 package models;
 
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+
 public class Cupom {
-	private String codigo;
-	private boolean automatico;
+	public String codigo;
+	public Date data;
+	public double valor;
+	public double valorSemDesconto;
+	public double desconto;
+	public double valorTotal;
+	
+	public Boolean validarCodigoDoCupom (String validarCodigo) {
+		if (validarCodigo.equals("ads0544")){
+			JOptionPane.showMessageDialog(null, "DESCONTO ADICIONADO!!");
+			return true;
+		}else {
+			JOptionPane.showMessageDialog(null, "CÓDIGO INVÁLIDO, INSIRA UM NOVO CÓDIGO!!");
+			return false;
+		}
+	}
+	
+	public double desconto (double valor) {
+		this.valor = (this.valorSemDesconto * this.desconto)/100;
+		return valor;
+	}
+	
+	public double valorTotalComDesconto (double valorTotal) {
+		this.valorTotal = this.valorSemDesconto - this.valor;
+		return valorTotal;
+	}
 	
 	public String getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(String codigo) {
 		this.codigo = codigo;
 	}
-	public boolean isAutomatico() {
-		return automatico;
+
+	public Date getData() {
+		return data;
 	}
-	public void setAutomatico(boolean automatico) {
-		this.automatico = automatico;
+
+	public void setData(Date data) {
+		this.data = data;
 	}
-	
-	
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public double getValorSemDesconto() {
+		return valorSemDesconto;
+	}
+
+	public void setValorSemDesconto(double valorSemDesconto) {
+		this.valorSemDesconto = valorSemDesconto;
+	}
+
+	public double getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(int desconto) {
+		this.desconto = desconto;
+	}
+
+	public double getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
 }
