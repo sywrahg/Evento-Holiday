fd48740a811400f35fc32cebcd4d5ea537353f61
sywrahg <sywrahgabriella@hotmail.com>
2017-08-28 16:42:37 -0300

    refatoração no método 'efetuarPagamento', agora chamado de 'pagar'.

--- src/models/Pagamento.java
+++ src/models/Pagamento.java
@@ -1,23 +1,24 @@
 package models;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class Pagamento {
 	private Date data;
-	private boolean pagamentoFeito;
 	private Usuario recebedor;
 	private double valorTotal;
-	private Atividade[] itensIncricao;
 	
-	public void CalculoPagamento(){
-		for (int i = 0; i < itensIncricao.length; i++) {
-			valorTotal += itensIncricao[i].getValor();
+	
+	public void CalculoPagamento(List<Atividade> itensIncricao){
+		for (Atividade atividade : itensIncricao) {
+			valorTotal += atividade.getValor();
 		}
 	}
 	
-	public void EfetuarPagamento(Usuario user){
-		pagamentoFeito = true;
+	public void Pagar(Usuario user){
 		recebedor = user;
+		data = Calendar.getInstance().getTime();
 	}
 	
 	public Date getData() {
@@ -25,12 +26,6 @@ public class Pagamento {
 	}
 	public void setData(Date data) {
 		this.data = data;
-	}
-	public boolean isPagamentoFeito() {
-		return pagamentoFeito;
-	}
-	public void setPagamentoFeito(boolean pagamentoFeito) {
-		this.pagamentoFeito = pagamentoFeito;
 	}
 	public Usuario getRecebedor() {
 		return recebedor;
@@ -45,14 +40,6 @@ public class Pagamento {
 
 	public void setValorTotal(double valorTotal) {
 		this.valorTotal = valorTotal;
-	}
-
-	public Atividade[] getItensIncricao() {
-		return itensIncricao;
-	}
-
-	public void setItensIncricao(Atividade[] itensIncricao) {
-		this.itensIncricao = itensIncricao;
 	}
 	
 	
