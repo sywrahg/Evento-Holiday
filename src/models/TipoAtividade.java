1b6474e33d312a5fc7cdec8c94ae8e6b68edc57d
sywrahg <sywrahgabriella@hotmail.com>
2017-09-01 23:45:49 -0300

    Add em TipoAtividade o tipo 'PADRAO'

--- src/models/TipoAtividade.java
+++ src/models/TipoAtividade.java
@@ -1,7 +1,7 @@
 package models;
 
 public enum TipoAtividade {
-	PALESTRA(1), MINICURSO(2),MESA_REDONDA(3), BATE_PAPO(4);
+	PALESTRA(1), MINICURSO(2),MESA_REDONDA(3), BATE_PAPO(4), PADRAO(5);
 	
 	public int valorTipoAtividade;
 	
